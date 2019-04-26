74fbeffa5a734b686cbccf944e2482fb94e4356e
Crawford <crawford.collins@gmail.com>
2019-04-26 11:00:36 -0500

    app is ready to deploy

--- network-demo/app/app.py
+++ network-demo/app/app.py
@@ -14,7 +14,7 @@ TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),config('TWITTER_ACC
 TWITTER = tweepy.API(TWITTER_AUTH)
 
 
-def get_first_user_interactions(search):
+def get_first_user_interactions(search,index):
 	twitter_user =TWITTER.get_user(search)
 	tweets = twitter_user.timeline(
 					count =200,
@@ -30,9 +30,10 @@ def get_first_user_interactions(search):
 	b = b.lower()
 	b = b.replace(search, "")
 	#regex for extracting @twitter_handles as elements of a list
-	interactions=re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)',ba)
-	# return the most common  names
-	return [ i[0] for i in Counter(interactions).most_common(10) ]
+	index=re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)',b)
+		# return the most common  names
+	return [ i[0] for i in Counter(index).most_common(10) ]
+
 
 def get_first_user_connections(search, interactions_list):
 	twitter_user =TWITTER.get_user(search)
@@ -71,10 +72,14 @@ def create_app():
 	def get_network():
 		user = request.values["user"]
 		user = user.replace("@","")
-		index =  get_first_user_interactions(user)
 		interactions_list = []
+		index = []
+		index = get_first_user_interactions(user,index)
+		index = list(set([user] +index))
 		for i in index:
-			 get_first_user_connections(user, interactions_list)
+			 get_first_user_connections(i, interactions_list)
+		#add first item to the top of the list
+
 		#turn the interactions into a dataframe
 		vectorizer = CountVectorizer(min_df=2)
 		X = vectorizer.fit_transform(interactions_list)
@@ -89,15 +94,43 @@ def create_app():
 					pass
 		#create json nodes from graph
 		sl_nodes = nx.spectral_layout(DG)
-		nodes_dicts = [{"id": i, "label": i, "x":sl_nodes[i][0], "y":sl_nodes[i][1] } for i in list(sl_nodes) ]
+		node_list = list(sl_nodes)
+		node_x = [sl_nodes[i][0] for i in node_list]
+		node_y = [sl_nodes[i][1] for i in node_list]
+		node_x = json.dumps(node_x)
+		node_y = json.dumps(node_y)
+		def get_column_sum(i):
+			try:
+				return int(display_df[i].sum())
+			except:
+				return 1
+		node_weights = [ get_column_sum(i) for i in node_list ]
+		node_weights = json.dumps(node_weights)
+		node_list = json.dumps(node_list)
+
 		#create json edges from graph
 		edges = list(DG.edges.data())
-		edges_dicts = [ {"id": edge[0]+" "+ edge[1],"source": edge[0],"target":edge[1]} for edge in edges]
-		edge_json = { "edges" : edges_dicts}
-		nodes_json = json.dumps(nodes_dicts)
-		edges_json = json.dumps(edges_dicts)
-		return render_template("network.html", nodes = nodes_json, edges = edges_json,
-		user = user)
+		sl_edges_x = []
+		sl_edges_y = []
+		sl_edges_weight = []
+		for i in range(len(edges)):
+		    sl_edges_x.append(sl_nodes[edges[i][0]][0])
+		    sl_edges_y.append(sl_nodes[edges[i][0]][1])
+		    sl_edges_x.append(sl_nodes[edges[i][1]][0])
+		    sl_edges_y.append(sl_nodes[edges[i][1]][1])
+		    sl_edges_weight.append(edges[i][2]["weight"])
+		    sl_edges_weight.append(edges[i][2]["weight"])
+		    sl_edges_x.append("")
+		    sl_edges_y.append("")
+		    sl_edges_weight.append("")
+		x = json.dumps(sl_edges_x)
+		y = json.dumps(sl_edges_y)
+		weights = json.dumps(sl_edges_weight)
+
+		user = json.dumps(user)
+		return render_template("network.html", edge_x =x, edge_y =y, node_x =
+		node_x, node_y =node_y, user = user, node_list=node_list,
+		node_weights=node_weights)
 
 
 
