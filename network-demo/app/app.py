0a4b104ed8e3e5f92dfdef7482b8641346ce843f
crawford <crawford.collins@gmail.com>
2019-05-17 01:28:26 -0500

    updated main.py to work with new gcf

--- network-demo/app/app.py
+++ network-demo/app/app.py
@@ -12,156 +12,90 @@ from collections import Counter
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 import nest_asyncio
+
 nest_asyncio.apply()
 
-#set up twitter environment
-TWITTER_AUTH =tweepy.OAuthHandler(config('TWITTER_CONSUMER_KEY'),config('TWITTER_CONSUMER_SECRET'))
-TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),config('TWITTER_ACCESS_TOKEN_SECRET'))
-TWITTER = tweepy.API(TWITTER_AUTH)
 
+def get_network(user):
+    headers = {"Content-Type": "application/json"}
+    body = {
+        "TWITTER_ACCESS_TOKEN": config("TWITTER_ACCESS_TOKEN"),
+        "TWITTER_ACCESS_TOKEN_SECRET": config("TWITTER_ACCESS_TOKEN_SECRET"),
+        "search": user,
+    }
+    edge_list = requests.post(
+        url="https://us-central1-twitter-bert-models.cloudfunctions.net/function-1",
+        headers=headers,
+        data=json.dumps(body),
+    )
+    edge_list = edge_list.json()
+    edge_df = pd.DataFrame(edge_list, columns=["source", "target"])
+    # create a graph and add edges
+    DG = nx.MultiGraph()
+    DG = nx.from_pandas_edgelist(edge_df, "source", "target")
+    DG_layout = nx.spectral_layout(DG)
 
-def get_first_user_interactions(search,index):
-    twitter_user =TWITTER.get_user(search)
-    tweets = twitter_user.timeline(
-				count =200,
-				exclude_replies = False,
-				include_rts=True,
-				tweet_mode='extended'
-				)
-    # generate a  list of tweets. join it to string. extract user names. turn
-    # it into a string again.
-    #b is a transistion variable
-    b = [ i.full_text for i in tweets ]
-    b = " ".join(b)
-    b = b.lower()
-    b = b.replace(search, "")
-    #regex for extracting @twitter_handles as elements of a list
-    index=re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)',b)
-        # return the most common  names
-    return [ i[0] for i in Counter(index).most_common(25) ]
+    # create json nodes from graph
+    sl_nodes = nx.spectral_layout(DG)
+    node_list = list(sl_nodes)
+    node_x = [sl_nodes[i][0] for i in node_list]
+    node_y = [sl_nodes[i][1] for i in node_list]
+    node_x = json.dumps(node_x)
+    node_y = json.dumps(node_y)
 
+    node_counter = Counter(list(chain.from_iterable(edge_list)))
+    node_keys = list(sl_nodes.keys())
+    node_weights = [node_counter[i] for i in node_keys]
+    node_weights = json.dumps(node_weights)
+    node_list = json.dumps(node_list)
 
-def get_first_user_connections_multi_process(session,search,
-        interactions_list,index):
-    try:
-        twitter_user =TWITTER.get_user(search)
-        tweets = twitter_user.timeline(
-                        count =200,
-                        exclude_replies = False,
-                        include_rts=True,
-                        tweet_mode='extended'
-                        )
-        #b is a transistion variable
-        b = [ i.full_text for i in tweets ]
-        b = " ".join(b)
-        b = b.lower()
-        b = b.replace(search, "")
-        #regex for extracting @twitter_handles as elements of a list
-        interactions = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9-_]+)',b)
-        # turn the list to string and add it to a list
-        interactions =" ".join(interactions)
-        interactions_list.append(interactions)
-    except:
-       index.remove(search)
-    finally:
-        return index,interactions_list
-async def get_data_asynchronous(index,interactions_list):
-    with ThreadPoolExecutor(max_workers =40) as executor:
-        with requests.Session() as session:
-            #initialize the event loop
-            loop = asyncio.get_event_loop()
+    # create json edges from graph
+    edges = list(DG.edges.data())
+    sl_edges_x = []
+    sl_edges_y = []
+    sl_edges_weight = []
+    for i in range(len(edges)):
+        sl_edges_x.append(sl_nodes[edges[i][0]][0])
+        sl_edges_y.append(sl_nodes[edges[i][0]][1])
+        sl_edges_x.append(sl_nodes[edges[i][1]][0])
+        sl_edges_y.append(sl_nodes[edges[i][1]][1])
+        sl_edges_x.append("")
+        sl_edges_y.append("")
+        sl_edges_weight.append("")
+    x = json.dumps(sl_edges_x)
+    y = json.dumps(sl_edges_y)
+    weights = json.dumps(sl_edges_weight)
 
-            #create a list of tasks with list comprehension.
-            #The executor function will run
-            #get_first_user_connections_multi_process
-            tasks = [ loop.run_in_executor(
-                executor, get_first_user_connections_multi_process,*(session, i,
-                    interactions_list, index) ) for i in index]
-            #initialize the tasks to run and await the result
-            for response in await asyncio.gather(*tasks):
-                pass
+    user = json.dumps(user)
 
-def execute_async_event_loop(index, interactions_list):
-    """this function does something analogous to compiling the
-    get_data_asynchronously  function and executes it.
-    """
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    future = asyncio.ensure_future(get_data_asynchronous(index,
-        interactions_list))
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(future)
+    #         return render_template("network.html", edge_x =x, edge_y =y, node_x =
+    #         node_x, node_y =node_y, user = user, node_list=node_list,
+    #         node_weights=node_weights)
+    return x, y, node_x, node_y, user, node_list, node_weights
 
-    return index, interactions_list
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-
-    @app.route('/')
+    @app.route("/")
     def root():
         return render_template("root.html")
 
-    @app.route('/network', methods = ["GET"])
-    def get_network():
+    @app.route("/network", methods=["GET"])
+    def network():
         user = request.values["user"]
-        user = user.replace("@","")
-        interactions_list = []
-        index = []
-        index = get_first_user_interactions(user,index)
-        index = list(set([user] +index))
-        index, interactions_list = execute_async_event_loop(index, interactions_list)
-
-        vectorizer = CountVectorizer()
-        X = vectorizer.fit_transform(interactions_list)
-        df = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names(),index= index)
-        DG = nx.MultiGraph()
-        for i in df.index.values:
-            for j in df.columns:
-                if df[j].loc[i] >0:
-                    DG.add_edge(j,i,weight = np.sqrt(df[j].loc[i] ))
-                else:
-                    pass
-        #create json nodes from graph
-        sl_nodes = nx.spectral_layout(DG)
-        node_list = list(sl_nodes)
-        node_x = [sl_nodes[i][0] for i in node_list]
-        node_y = [sl_nodes[i][1] for i in node_list]
-        node_x = json.dumps(node_x)
-        node_y = json.dumps(node_y)
-        def get_column_sum(i):
-            try:
-                return int(df[i].sum())
-            except:
-                return 1
-        node_weights = [ get_column_sum(i) for i in node_list ]
-        node_weights = json.dumps(node_weights)
-        node_list = json.dumps(node_list)
-
-        #create json edges from graph
-        edges = list(DG.edges.data())
-        sl_edges_x = []
-        sl_edges_y = []
-        sl_edges_weight = []
-        for i in range(len(edges)):
-            sl_edges_x.append(sl_nodes[edges[i][0]][0])
-            sl_edges_y.append(sl_nodes[edges[i][0]][1])
-            sl_edges_x.append(sl_nodes[edges[i][1]][0])
-            sl_edges_y.append(sl_nodes[edges[i][1]][1])
-            sl_edges_weight.append(edges[i][2]["weight"])
-            sl_edges_weight.append(edges[i][2]["weight"])
-            sl_edges_x.append("")
-            sl_edges_y.append("")
-            sl_edges_weight.append("")
-        x = json.dumps(sl_edges_x)
-        y = json.dumps(sl_edges_y)
-        weights = json.dumps(sl_edges_weight)
-
-        user = json.dumps(user)
-        return render_template("network.html", edge_x =x, edge_y =y, node_x =
-        node_x, node_y =node_y, user = user, node_list=node_list,
-        node_weights=node_weights)
-
-
+        user = user.replace("@", "")
+        x, y, node_x, node_y, user, node_list, node_weights = get_network(user)
+        return render_template(
+            "network.html",
+            edge_x=x,
+            edge_y=y,
+            node_x=node_x,
+            node_y=node_y,
+            user=user,
+            node_list=node_list,
+            node_weights=node_weights,
+        )
 
     return app
